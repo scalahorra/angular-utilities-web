8cf4de1389b97d2bb9e7e5ae9a7b514362b80e3d
Sergio Calahorra <sergiocalahorra20@gmail.com>
2024-06-02 02:13:45 +0200

    Update Angular from 15 to 18

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,18 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
+  standalone: true,
+  imports: [],
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
-  constructor() { }
-
-  utilities: any[] = [
-    {
-      title: 'cambiar extensión archivos',
-      action: 'change-file-extension'
-    }
-  ]
 
 }
